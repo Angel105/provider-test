578f1f0641671ed74e06537f919fed49a259ecc4
Borzoi <siteandstock@gmail.com>
2025-02-22 14:03:45 +0100

    [0] initial commit : ProviderTest attempts to read data from DatabaseTest and gets a blocking error (in Logcat): AppsFilter interaction: PackageSetting {8f51eda com.example.providertest/ 10210} -> PackageSetting{d5e6a6d com.example.databasetest/ 10209}  BLOCKED
    As well as
    
    ActivityThread com.example.providertest Failed to find provider info for com.example.databasetest. provider

---
+++ app/src/main/java/com/example/providertest/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.providertest
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.contentValuesOf
+import com.example.providertest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    var bookId: String? = null
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addData.setOnClickListener {
+            // add data
+            val uri = Uri.parse("content://com.example.databasetest.provider/book")
+            val values = contentValuesOf("name" to "A Clash of Kings",
+                "author" to "George Martin", "pages" to 1040, "price" to 22.85)
+
+            val newUri = contentResolver.insert(uri, values)
+            bookId = newUri?.pathSegments?.get(1)
+        }
+
+        binding.queryData.setOnClickListener {
+            // query data
+            val uri = Uri.parse("content://com.example.databasetest.provider/book")
+            contentResolver.query(uri, null, null, null, null)?.apply {
+                while (moveToNext()) {
+                    val name = getString(getColumnIndex("name"))
+                    val author = getString(getColumnIndex("author"))
+                    val pages = getInt(getColumnIndex("pages"))
+                    val price = getDouble(getColumnIndex("price"))
+                    Log.d("MainActivity", "Book name is $name, author is $author, pages is $pages, price is $price")
+                }
+                close()
+            }
+        }
+
+        binding.updateData.setOnClickListener {
+            // update data
+            bookId?.let {
+                val uri = Uri.parse("content://com.example.databasetest.provider/book/$it")
+                val values = contentValuesOf("name" to "A Storm of Swords","pages" to 1216, "price" to 24.05)
+                contentResolver.update(uri, values, null, null)
+            }
+        }
+
+        binding.deleteData.setOnClickListener {
+            // delete data
+            bookId?.let {
+                val uri = Uri.parse("content://com.example.databasetest.provider/book/$it")
+                contentResolver.delete(uri, null, null)
+            }
+        }
+
+    }
+}
